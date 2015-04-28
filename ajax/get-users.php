858956b5f804e24bf01e80e30864676e4bc4a9eb
@dantaylorseo <dan@tailored.im>
2015-04-28 20:43:59 +0100

    Add users

--- ajax/get-users.php
+++ ajax/get-users.php
@@ -1,4 +1,22 @@
 <?php
+	header('Access-Control-Allow-Origin: *');
+	header('Cache-Control: no-cache, must-revalidate');
 	
+	require( '../inc/config.php' );
 	
-	echo 'Hello';
+	$query = "SELECT * FROM user ORDER BY userName ASC";
+	$result = $db->query( $query );
+	
+	$output = array();
+	
+	while( $item = $result->fetch_assoc() ) {
+		$output[$item['userID']] = array (
+			'userID' 		=> $item['userID'],
+			'userName' 		=> $item['userName'],
+			'userEmail' 	=> $item['userEmail'],
+			'userPass' 		=> $item['userPass'],
+			'userType' 		=> $item['userType'],
+		);		
+	}
+	
+	echo json_encode( (object) $output );
