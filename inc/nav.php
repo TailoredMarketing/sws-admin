f71a51431cd8c4dd226eb05d83632d867d076273
Dan Taylor <DanTaylor@Dans-Mac-Pro.local>
2015-07-13 12:35:22 +0100

    merges

--- inc/nav.php
+++ inc/nav.php
@@ -32,23 +32,6 @@
             <li><a href="#">One more separated link</a></li>
           </ul>
         </li>
-        <li class="dropdown <?php echo ( isset( $page['page'] ) && $page['page'] == 'clients' ? ' active "' : '' ); ?>"> <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Clients <span class="caret"></span></a>
-          <ul class="dropdown-menu" role="menu">
-            <li><a href="#">Action</a></li>
-            <li><a href="#">Another action</a></li>
-            <li><a href="#">Something else here</a></li>
-            <li class="divider"></li>
-            <li><a href="#">Separated link</a></li>
-            <li class="divider"></li>
-            <li><a href="#">One more separated link</a></li>
-          </ul>
-        </li>
-        <li class="dropdown <?php echo ( isset( $page['page'] ) && $page['page'] == 'users' ? ' active "' : '' ); ?>"> <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Users <span class="caret"></span></a>
-          <ul class="dropdown-menu" role="menu">
-            <li><a href="/users/view/">View Users</a></li>
-            <li><a href="/users/new/">New User</a></li>
-          </ul>
-        </li>
       </ul>
       <ul class="nav navbar-nav navbar-right">
         <li class="dropdown"> <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><i class="fa fa-user"></i> Dan Taylor <span class="caret"></span></a>
