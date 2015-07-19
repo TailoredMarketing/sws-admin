3f6aa8cce201a2f7d12bff4f15510ffdf8e70ed3
Dan Taylor <DanTaylor@Dans-Mac-Pro.local>
2015-07-19 13:22:53 +0100

    add obs fixed

---
+++ ajax/add-obs.php
@@ -0,0 +1,27 @@
+<?php
+	header('Access-Control-Allow-Origin: *');
+	header('Cache-Control: no-cache, must-revalidate');
+	
+	require( '../inc/config.php' );
+	
+	$output = array();
+	
+	$obsID 			= $db->real_escape_string( $_POST['obsID'] );
+	$obsReport 		= $db->real_escape_string( $_POST['obsReport'] );
+	$obsItem 		= $db->real_escape_string( $_POST['obsItem'] );
+	$obsObs 			= $db->real_escape_string( $_POST['obsObs'] );
+	$obsPriority 	= $db->real_escape_string( $_POST['obsPriority'] );
+	
+	$query = "INSERT INTO obs ( obsID, obsReport, obsItem, obsItem, obsObs, obsPriority v) VALUES ( '$obsID', '$obsReport', '$obsItem', '$obsObs', '$obsPriority' )";
+	
+	if( $db->query( $query ) ) {
+		$output = array(
+			'status' => 1
+		);
+	} else {
+		$output = array(
+			'status' => 0
+		);
+	}
+		
+	echo json_encode( (object) $output );
